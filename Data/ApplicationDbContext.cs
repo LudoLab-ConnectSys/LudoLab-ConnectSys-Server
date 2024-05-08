13f1d01fd429f904551270a024e5bb7942937da2
ConstruexDevelopment <149097574+ConstruexDevelopment@users.noreply.github.com>
2024-05-07 21:39:10 -0500

    add new proyect front
    add new proyect front

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DirectorioDeArchivos.Shared;
+using Microsoft.EntityFrameworkCore;
+using DirectorioDeArchivos.Shared;
+using System.Collections.Generic;
+
+namespace LudoLab_ConnectSys_Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<UploadResult> UploadResults => Set<UploadResult>();
+    }
+}
