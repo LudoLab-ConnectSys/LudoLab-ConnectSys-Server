2b6070c5979dcf21d0cd52f25f416590808ad0fe
MiguelMuenala <mickaelo.1755@gmail.com>
2024-08-20 00:37:59 -0500

    cambio en Program.cs
    cambio en Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,24 @@
-using LudoLab_ConnectSys_Server.Data;
+ï»¿using LudoLab_ConnectSys_Server.Data;
+using LudoLab_ConnectSys_Server.Interceptors;
 using LudoLab_ConnectSys_Server.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging; // Agrega este using
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
-
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddSingleton<TokenService>();
 // Registrar GrupoService
 builder.Services.AddScoped<GrupoService>();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Registrar IHttpContextAccessor para AuditService
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("DevCorsPolicy", builder =>
@@ -28,6 +29,14 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader();
     });
 });
+
+// Registrar HttpCliente para AuditService
+builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<AuditSaveChangesInterceptor>();
+builder.Services.AddLogging(); // Configura el logging
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -40,14 +49,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
-app.UseStaticFiles(); // Sirve archivos estáticos desde wwwroot por defecto
-// Configurar archivos estáticos para la carpeta uploads
+app.UseStaticFiles(); // Sirve archivos estaticos desde wwwroot por defecto
+// Configurar archivos estï¿½ticos para la carpeta uploads
 var uploadsPath = Path.Combine(app.Environment.ContentRootPath, "uploads");
 if (!Directory.Exists(uploadsPath))
 {
     Directory.CreateDirectory(uploadsPath);
 }
+
 app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = new PhysicalFileProvider(uploadsPath),
@@ -59,7 +68,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.UseCors("DevCorsPolicy");
-
-
 
 app.Run();
